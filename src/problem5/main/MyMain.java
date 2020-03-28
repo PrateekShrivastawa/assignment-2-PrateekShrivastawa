b8aedbc5f78feaa31204cdd16398270e66de28dc
PrateekShrivastawa <prateek.shrivastawa_cs18@gla.ac.in>
2020-03-28 19:19:02 +0530

    init commit

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -5,6 +5,34 @@
  *  Time: 7:06 PM
  */
 package problem5.main;
+
+import problem5.circularqueue.MyCircularQueue;
+import problem5.student.Student;
+
+import java.util.Scanner;
+
 //executable class
 public class MyMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        MyCircularQueue myCircularQueue;
+        myCircularQueue = new MyCircularQueue();
+        System.out.println("Enter the size of Student list");
+        int size = sc.nextInt();
+        sc.nextLine();
+        int arr[] = new int[size];
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            sc.nextLine();
+            System.out.println("ENTER THE STUDENT NAME");
+            String s = sc.nextLine();
+            System.out.println("ENTER THE NUMBER OF BACKLOGS");
+            int b = sc.nextInt();
+            Student student = new Student(s, b);
+            myCircularQueue.enqueue(student);
+        }
+        System.out.println(" so the original number of students" + myCircularQueue.getSize());
+        System.out.println("total no of students deleted=" + myCircularQueue.remove());
+        System.out.println("so the new size of queue" + (myCircularQueue.getSize() - myCircularQueue.remove()));
+    }
 }
